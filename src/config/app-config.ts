17e5423ee9436784218d574e32567fa26b5926e9
Claudia <cortezgabriela693@gmail.com>
2025-06-05 03:42:23 -0400

    qqqqq

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -6,6 +6,6 @@ export const AppConfig = {
   APP_VERSION: packageJson.version,
   APP_ENV: import.meta.env.MODE ?? 'development',
   // API_URL: import.meta.env.VITE_API_URL ?? 'http://localhost:8000/api'
- API_URL: import.meta.env.VITE_API_URL ?? ' https://servidor-ryib.onrender.com/api'
+ API_URL: import.meta.env.VITE_API_URL ?? 'https://servidor-ryib.onrender.com/api'
  
 };
